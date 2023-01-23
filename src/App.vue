b1ce49737ce1d8b9e009cd24033932688a2d111f
Your Name <you@example.com>
2023-01-23 22:37:28 +0700

    Proweb

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+// coding javascript disini
+
+export default{
+  data(){
+    return{
+      datasensor: "60",
+      datasensor2: "80"
+    }
+  },
+  created(){
+    //this.getData()
+  },
+  methods:{
+    async getData(){
+      const data = await fetch("http://localhost/sensor1");
+      const datajson = await data.json();
+      this.datasensor = datajson.sensor1
+      //this.datasensor = 100
+    },
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    Hello {{ datasensor }}
+    <button @click="getData" class="bg-gradient-to-r from-cyan-500 to-blue-500 p-2 rounded-xl">Ambil Data</button>&nbsp;
+    <button @click="getData" class="bg-gradient-to-r from-cyan-500 to-blue-500 p-2 rounded-3xl">Ambil Data 2</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,3 @@
+ <template>
+    <h1>    Hello HelloWorld</h1>
+ </template>
